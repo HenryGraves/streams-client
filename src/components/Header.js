3cf51bb643afeb57c365630c3d15d5d7fcfc8d4b
Henry <henrygraves95@gmail.com>
2020-05-19 21:00:47 -0700

    modified header size and logos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeadset } from '@fortawesome/free-solid-svg-icons'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+
+
 import GoogleAuth from './GoogleAuth';
 
 const Header = () => {
   return (
     <div className="ui secondary pointing menu">
-      <Link to='/' className="item">
-        Glide
+      <Link to='/' className="item" style={{fontSize: 30}}>
+      <i class="fab fa-glide"></i> &nbsp; Glide 
       </Link>
       <div className="right menu">
-        <Link to="/" className="item">
-          All Streams
+        <Link to="/" className="item" style={{fontSize: 25}}>
+        <FontAwesomeIcon icon={faHome}/>
         </Link>
-        <GoogleAuth />
+        <div className="item">
+          <GoogleAuth />
+          
+        </div>
+        
       </div>
     </div>
   );
